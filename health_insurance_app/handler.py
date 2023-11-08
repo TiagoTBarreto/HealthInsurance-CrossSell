e180a3d9a1ac602b27cd5e6c0eca20c9828d3022
TiagoTBarreto <tiagotavaresbarreto@gmail.com>
2023-11-08 08:22:47 -0300

    port 5000 added

--- health_insurance_app/handler.py
+++ health_insurance_app/handler.py
@@ -4,7 +4,7 @@ from healthinsurance.HealthInsurance import HealthInsurance
 import pickle 
 
 # loading model
-model = pickle.load( open('/home/tiagobarreto/DS/repos/health-insurance1/model/model_health_insurance.pkl', 'rb'))
+model = pickle.load( open('model/model_health_insurance.pkl', 'rb'))
 
 app = Flask(__name__)
 
@@ -43,4 +43,5 @@ def health_insurance_predict():
         return Response('{}', status = 200, mimetype='application/json' )
     
 if __name__ == '__main__':
-    app.run( '0.0.0.0', debug = True )
+    port = os.environ.get('PORT', 5000)
+    app.run( '0.0.0.0', port = port, debug = True )
